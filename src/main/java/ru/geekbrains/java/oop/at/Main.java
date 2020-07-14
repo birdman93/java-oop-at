9acf25dc56b081cec73af85a4de5f7b1dbbecc4c
Denis <47885411+birdman93@users.noreply.github.com>
2020-07-14 12:36:34 +0300

    .

---
+++ src/main/java/ru/geekbrains/java/oop/at/Main.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.java.oop.at;
+import java.util.Random; // будем генерировать действия и параметры действий
+
+public class Main {
+
+    public static void main(String[] args) {
+
+/* Нагенерируем доступные параметры действий для каждого из 3-х персонажей*/
+    Random random = new Random();
+
+
+
+
+
+    }
+}
