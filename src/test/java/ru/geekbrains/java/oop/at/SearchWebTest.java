e89094884dff8bb56cf000ee82be8684305e37a8
Denis <47885411+birdman93@users.noreply.github.com>
2020-07-22 18:26:23 +0600

    hm_4

--- src/test/java/ru/geekbrains/java/oop/at/SearchWebTest.java
+++ src/test/java/ru/geekbrains/java/oop/at/SearchWebTest.java
@@ -15,13 +15,17 @@ package ru.geekbrains.java.oop.at;
 В Проектах и компаниях отображается GeekBrains
 */
 
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import ru.geekbrains.java.oop.at.base.BaseWebTest;
 
+import static org.hamcrest.Matchers.*;
+
 public class SearchWebTest extends BaseWebTest {
 
-    @Test
+    @BeforeEach
     public void checkSearch() throws InterruptedException {
 
         // Выполнил задание с выводом результатов в консоль
@@ -37,68 +41,66 @@ public class SearchWebTest extends BaseWebTest {
         // Вводим в поисковую строку "java"
         chromeDriver.findElementByCssSelector("input[class=\"search-panel__search-field\"]")
                 .sendKeys("java");
+    }
 
-        // Проверяем, что профессий не менее 2-х
+    // Проверяем, что профессий не менее 2-х
+    @Test
+    void professions() {
         String professions_word = chromeDriver.findElement(By.cssSelector("[class=\"search-page-tabs\"] [data-tab=\"professions\"] span")).getText();
         int professions_count = Integer.parseInt(professions_word);
-        if (professions_count >= 2) {
-            System.out.println("Профессий НЕ менее чем 2");
-        } else {
-            System.out.println("Профессий менее чем 2");
-        }
+        MatcherAssert.assertThat(professions_count, greaterThan(2));
+    }
 
         // Проверяем, что курсов более 15
+    @Test
+    void courses() {
         String courses_word = chromeDriver.findElement(By.cssSelector("[class=\"search-page-tabs\"] [data-tab=\"courses\"] span")).getText();
         int courses_count = Integer.parseInt(courses_word);
-        if (courses_count > 15) {
-            System.out.println("Курсов более 15");
-        } else {
-            System.out.println("Курсов НЕ болеее 15");
-        }
+        MatcherAssert.assertThat(courses_count, greaterThan(15));
+    }
 
         // Проверяем, что вебинаров больше чем 180, но меньше 300
+    @Test
+    void webinars() {
         String webinars_word = chromeDriver.findElement(By.cssSelector("[class=\"search-page-tabs\"] [data-tab=\"webinars\"] span")).getText();
         int webinars_count = Integer.parseInt(webinars_word);
-        if ((webinars_count > 180) && (webinars_count < 300)) {
-            System.out.println("Вебинаров больше чем 180, но меньше 300");
-        } else {
-            System.out.println("Вебинаров НЕ больше чем 180, но меньше 300");
-        }
-
-        // Проверяем, что в вебинарах отображается первым “Java Junior. Что нужно знать для успешного собеседования?”
-        // Найти легко, но вот с проверкой, что этот вебинар первый, я не совладал.
-        chromeDriver.findElement(By.xpath(".//*[text()='Java Junior. Что нужно знать для успешного собеседования?']/.."));
-
+        MatcherAssert.assertThat(webinars_count, greaterThan(180));
+        MatcherAssert.assertThat(webinars_count, lessThan(300));
+    }
 
         // Проверяем, что блогов более 300
+    @Test
+    void blogs() {
         String blogs_word = chromeDriver.findElement(By.cssSelector("[class=\"search-page-tabs\"] [data-tab=\"blogs\"] span")).getText();
         int blogs_count = Integer.parseInt(blogs_word);
-        if (blogs_count > 300) {
-           System.out.println("Блогов более 300");
-        } else {
-            System.out.println("Блогов НЕ более 300");
-        }
+        MatcherAssert.assertThat(blogs_count, greaterThan(300));
+    }
 
         // Проверяем, что форумов не 350
+    @Test
+    void forums() {
         String forums_word = chromeDriver.findElement(By.cssSelector("[class=\"search-page-tabs\"] [data-tab=\"forums\"] span")).getText();
         int forums_count = Integer.parseInt(forums_word);
-        if (forums_count != 350) {
-            System.out.println("Форумов НЕ 350");
-        } else {
-            System.out.println("Форумов 350");
-        }
+        MatcherAssert.assertThat(forums_word, not(350));
+    }
 
         // Проверяем, что тестов не 0
+    @Test
+    void tests() {
         String tests_word = chromeDriver.findElement(By.cssSelector("[class=\"search-page-tabs\"] [data-tab=\"tests\"] span")).getText();
         int tests_count = Integer.parseInt(tests_word);
-        if (tests_count != 0) {
-            System.out.println("Тестов НЕ 0");
-        } else {
-            System.out.println("Тестов 0");
-        }
-
+        MatcherAssert.assertThat(tests_count, not(0));
+    }
         // Проверяем, что в Проектах и компаниях отображается GeekBrains (тут без вывода рез-та)
+    @Test
+    void GeekBrains () {
         chromeDriver.findElement(By.cssSelector("h3 [href=\"/career/682\"]"));
+    }
+
+    // Проверяем, что в вебинарах отображается первым “Java Junior. Что нужно знать для успешного собеседования?”
+    @Test
+    void Java_Junior () {
+        chromeDriver.findElement(By.xpath(".//*[text()='Java Junior. Что нужно знать для успешного собеседования?']/.."));
     }
 }
 
